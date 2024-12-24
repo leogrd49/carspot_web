efe42de6381f0607c76407afd7cb7cbf15f0c9b0
Gabriel <gabriel.delechamp.pro@gmail.com>
2024-12-24 12:37:14 +0100

    logo selected while on the page

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,5 @@
-import { Link } from 'react-router-dom';
 import Logo from '../assets/LOGO_TRANSPERENT_.png';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   const navItems = [
@@ -16,28 +16,32 @@ const Navbar = () => {
         <ul className="flex flex-col space-around h-full justify-around items-start w-full">
           {navItems.map((item) => (
             <li key={item.path} className="w-full">
-              <Link
-                to={item.path}
-                className="hover:text-[#FFC710] transition-colors flex items-center gap-4 p-2 pl-5 relative group"
-              >
+              <NavLink
+                  to={item.path}
+                  className={({ isActive }) => 
+                    `transition-colors flex items-center gap-4 p-2 pl-5 relative group ${
+                      isActive ? 'text-[#FFC710]' : 'hover:text-[#FFC710]'
+                    }`
+                  }
+                >
                 <div className="min-w-6">
                 {item.label =="Accueil" && 
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                 </svg>
                 }
                 {item.label =="Dashboard" && 
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25a2.25 2.25 0 0 1-2.25-2.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z" />
                 </svg>
                 }
                 {item.label =="Analytics" && 
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 14.25v2.25m3-4.5v4.5m3-6.75v6.75m3-9v9M6 20.25h12A2.25 2.25 0 0 0 20.25 18V6A2.25 2.25 0 0 0 18 3.75H6A2.25 2.25 0 0 0 3.75 6v12A2.25 2.25 0 0 0 6 20.25Z" />
                 </svg>
                 }
                 {item.label =="Logs" && 
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="m6.75 7.5 3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0 0 21 18V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v12a2.25 2.25 0 0 0 2.25 2.25Z" />
                 </svg>
               
@@ -46,7 +50,7 @@ const Navbar = () => {
                 <span className="opacity-0 nav:opacity-100 transition-all duration-300 whitespace-nowrap">
                   {item.label}
                 </span>
-              </Link>
+              </NavLink>
             </li>
           ))}
         </ul>
