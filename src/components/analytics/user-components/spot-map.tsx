d71a5b88f3832ad167439b03ca9f8ebde97a3ca5
Gabriel Delechamp <gabriel.delechamp.pro@gmail.com>
2025-02-18 16:32:21 +0100

    marker move

--- src/components/analytics/user-components/spot-map.tsx
+++ src/components/analytics/user-components/spot-map.tsx
@@ -48,7 +48,7 @@ const SpotMap = () => {
       }).addTo(mapRef.current);
     }
     var myIcon = L.icon({
-        iconUrl: 'marker.png',
+        iconUrl: 'src/assets/marker.png',
         iconSize: [38, 45],
 
     });
